377dfe2b7e8692a05bafdd60273c228409d843e6
Hasnain Motagamwala <husain.regal@gmail.com>
2023-05-17 20:43:44 +0530

    Binary Search Implementation

--- Problem_Solving_With_DSA/src/main/scala/in/olc/problem_solving/Searching/_1_1_BinarySearch.scala
+++ Problem_Solving_With_DSA/src/main/scala/in/olc/problem_solving/Searching/_1_1_BinarySearch.scala
@@ -1,23 +1,29 @@
 package in.olc.problem_solving.Searching
 
 object _1_1_BinarySearch {
-  def binSearch(arr: Array[Int], elem: Int): String = {
+  def binSearch(arr: Array[Int], data: Int): Option[Int] = { // arr = 71,89 | data = 71
     var left = 0
-    var right = arr.length - 1
-    while(left<right){
-      val mid = (left + right)/2
-      if(elem == arr(mid))
-        return s"Element Found at index: $mid"
-      else if(elem < arr(mid))
-        right = mid-1
+    var right = arr.length - 1 // 9
+    while (left < right) { //midVal = 63
+      val mid = (left + right) / 2 // 4
+      if (data > arr(mid)) {
+        left = mid + 1 // 8
+      }
+      else if (data < arr(mid)) {
+        right = mid - 1
+      }
       else
-        left = mid+1
+        return Some(mid)
     }
-    "Element Not Found"
+    None
   }
+
   def main(args: Array[String]): Unit = {
-    val arr = Array[Int](15,5,20,35,2,42,67,17)
-    println(binSearch(arr,5))
-    println(binSearch(arr,25))
+    //    val arr = Array(5,9,17,23,25,45,59,63,71,89)
+    val arr = Array[Int](15, 5, 20, 35, 2, 42, 67, 17)
+    binSearch(arr, 42) match {
+      case Some(num) => println(s"Element found at position: ${num + 1}")
+      case None => println("Element NOT Found.")
+    }
   }
 }
