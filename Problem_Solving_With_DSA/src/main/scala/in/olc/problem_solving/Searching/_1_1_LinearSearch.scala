af27d382f40f05e7093f2c30d914849a269c02ce
Hasnain Motagamwala <husain.regal@gmail.com>
2023-05-17 20:43:25 +0530

    Linear Search Implementation

--- Problem_Solving_With_DSA/src/main/scala/in/olc/problem_solving/Searching/_1_1_LinearSearch.scala
+++ Problem_Solving_With_DSA/src/main/scala/in/olc/problem_solving/Searching/_1_1_LinearSearch.scala
@@ -1,16 +1,19 @@
 package in.olc.problem_solving.Searching
 
 object _1_1_LinearSearch {
-  def search(arr: Array[Int], elem: Int): String = {
-    for(i <- arr.indices){
-      if(arr(i) == elem)
-        return s"Element Found at index: $i and  position: ${i+1}"
+  def findElem(arr: Array[Int], data: Int): Option[Int] = {
+    for(i <- arr){
+      if(i == data)
+        return Some(arr.indexOf(i))
     }
-    "Element Not Found"
+    None
   }
   def main(args: Array[String]): Unit = {
-    val arr = Array[Int](15,5,20,35,2,42,67,17)
-    println(search(arr,42))
-    println(search(arr,25))
+    val arr = Array(1,2,3,43,34,23)
+    val data = 43
+    findElem(arr,data) match {
+      case Some(num) => println(s"Element $data found at position: ${num + 1}")
+      case None => println("Element not found.")
+    }
   }
 }
