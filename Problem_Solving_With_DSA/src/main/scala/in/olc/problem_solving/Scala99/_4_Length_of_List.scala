4549e8f7732a0ab8d6cfdd38aae0ed38cf9ac606
Hasnain Motagamwala <husain.regal@gmail.com>
2023-05-19 16:05:30 +0530

    Find the length of a list with Tail Recursion

---
+++ Problem_Solving_With_DSA/src/main/scala/in/olc/problem_solving/Scala99/_4_Length_of_List.scala
@@ -0,0 +1,20 @@
+package in.olc.problem_solving.Scala99
+
+import scala.annotation.tailrec
+
+object _4_Length_of_List {
+
+  def length(li: List[Int]): Int = {
+    @tailrec
+    def tailReclength(len: Int, li: List[Int]): Int = li match {
+      case Nil => len
+      case _::t => tailReclength(len + 1, t)
+    }
+    tailReclength(0,li)
+  }
+  def main(args: Array[String]): Unit = {
+    val li = List(1, 1, 2, 3, 5, 8)
+    println(length(li))
+
+  }
+}
